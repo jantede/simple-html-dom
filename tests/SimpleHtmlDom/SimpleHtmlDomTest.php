033635d35273c322866990ead78ef017a092109d
Tede Mehrtens <tede@mehrtens.me>
2015-10-09 16:45:02 +0200

    added more tests

--- tests/SimpleHtmlDom/SimpleHtmlDomTest.php
+++ tests/SimpleHtmlDom/SimpleHtmlDomTest.php
@@ -3,12 +3,21 @@
 	
 	class SimpleHtmlDomTest extends PHPUnit_Framework_TestCase
 	{
-		public function testFindTag()
+		public function testFindElement()
 		{
 			$dom = new SimpleHtmlDom;
 			$dom->load('<html><head><title>Test</title></head><body><p id="test">Some Content and <a href="http://example.org">a link</a></p><p id="test2>A second text</p></body></html>');
 			
-			$this->assertEquals('Te3st', $dom->find('title', 1)->innerText);
+			$this->assertEquals('Test', $dom->find('title', 1)->innerText);
 			$this->assertEquals('Some Content and a link', $dom->find('p[id=test]')->plainText);
+			$this->assertEquals('http://example.org', $dom->find('p[id=test] a')->href); 
+		}
+		
+		public function testFindElementNested()
+		{
+			$dom = new SimpleHtmlDom;
+			$dom->load('<html><head><title>Test</title></head><body><p id="test">Some Content and <a href="http://example.org">a link</a></p><p id="test2>A second text</p></body></html>');
+			
+			$this->assertEquals('http://example.org', $dom->find('p[id=test]')->find('a')->href);
 		}
 	}
